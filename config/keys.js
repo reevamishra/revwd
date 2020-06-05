a60a63c525f3aaa0dd9d5508a1d0d663ce640a3e
reevamishra <reevamishra208@gmail.com>
2020-06-05 22:53:39 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//clientID 
+//clientSecret 
+
+module.exports = {
+    googleClientID: '981037200011-cnla7ns4p9s2rbd5bhcgjleumfbb7sr1.apps.googleusercontent.com',
+    googleClientSecret: '-c6u5H5N19oeds0rAsUw5aF9',
+    mongoURI: 'mongodb+srv://emaily-dev:KhwCmRIlsxMJp1uj@emaily-qyjdy.mongodb.net/emaily?retryWrites=true&w=majority'
+};
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+const app = express();
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;  //to take port from heroku 
+app.listen(PORT);
