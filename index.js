6d62a23f2bea7c8d6bb098d0c01ed6490be71ae3
reevamishra <reevamishra208@gmail.com>
2020-06-07 10:40:34 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const keys = require('./config/keys');
 const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
+
 const app = express();
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000;  //to take port from heroku 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -5,12 +5,15 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-    }, 
-    (accessToken, refreshToken, profile, done) => {
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+      },
+      (accessToken, refreshToken, profile, done) => {
         new User({ googleId: profile.id }).save();
-    })
-);
+      }
+    )
+  );
