0347309fd7cb2e4d0bb46d0aeed0cda9f7c2bcbd
reevamishra <reevamishra208@gmail.com>
2020-06-25 20:40:23 +0530

    Add files via upload

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>
+                revwd
+            </h1>
+            Collect feedback and get your product reviewed!
+        </div>
+    );
+};
+
+export default Landing;
