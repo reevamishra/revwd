e32c64e22573db16b073e66906277f0d30d35ced
reevamishra <reevamishra208@gmail.com>
2020-06-25 20:41:03 +0530

    Add files via upload

--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,9 @@ module.exports = {
     googleClientID: '981037200011-cnla7ns4p9s2rbd5bhcgjleumfbb7sr1.apps.googleusercontent.com',
     googleClientSecret: '-c6u5H5N19oeds0rAsUw5aF9',
     mongoURI: 'mongodb+srv://emaily-user:tQQjjhNl118SYZ6n@emaily-ylzzb.mongodb.net/emaily?retryWrites=true&w=majority',
-    cookieKey: 'njndsjcnjkdsncjndsjcnjkndjnsd'    
+    cookieKey: 'njndsjcnjkdsncjndsjcnjkndjnsd',
+    stripePublishableKey: 'pk_test_51GvF0TDxA6bLMWKjOZwA7u7tfPa6CP67omy6skJv3gO7C9oxlp4GGxwHjSGavFVCCUO5CxqFAU6Noq53z3dVz08y00bMlsUsdA',
+    stripeSecretKey: 'sk_test_51GvF0TDxA6bLMWKjC2FXgQTYm9ohiHyfxjOHzn82qOxNGKnmHencPYgisbQy8vIv9uS9RzL6jROwu9zZrIv4pzC000NDk4LQtG',
+    sendGridKey: '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',
+    redirectDomain: 'http://localhost:3000'   
 };
